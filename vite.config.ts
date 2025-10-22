c4dfe360667955b3eaa7ee0c51fd39c5140fb1b8
Michael Nathan Laswardi <michael.laswardi@binus.edu>
2025-10-22 16:01:02 +0700

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PORT = process.env.VITE_API_PORT;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: API_URL || 'http://localhost:8000',
+    //     changeOrigin: true,
+    //   }
+    // }
   },
   optimizeDeps: {
     include: ['pdfjs-dist']
